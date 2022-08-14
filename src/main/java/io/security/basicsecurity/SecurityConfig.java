9ce9505826cfc31c2e834ad6960aef230f03ab0d
hyeonse <dlgustp1487@naver.com>
2022-08-14 11:18:45 +0900

    13) 사이트 간 요청 위조 - CSRF, CsrfFilter

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -33,48 +33,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired UserDetailsService userDetailsService;
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
-		auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS", "USER");
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN", "SYS", "USER");
-	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.authorizeRequests()
-				.antMatchers("/login").permitAll()
-				.antMatchers("/user").hasRole("USER")
-				.antMatchers("/admin/pay").hasRole("ADMIN")
-				.antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
 				.anyRequest().authenticated();
 		http
 				.formLogin()
-				.successHandler(new AuthenticationSuccessHandler() {
-					@Override
-					public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-						RequestCache requestCache = new HttpSessionRequestCache();
-						SavedRequest savedRequest = requestCache.getRequest(request, response);
-						String redirectUrl = savedRequest.getRedirectUrl();
-						response.sendRedirect(redirectUrl);
-					}
-				});
+				;
 
 		http
-				.exceptionHandling()
-				/*.authenticationEntryPoint(new AuthenticationEntryPoint() {
-					@Override
-					public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-						response.sendRedirect("/login");
-					}
-				})*/
-				.accessDeniedHandler(new AccessDeniedHandler() {
-					@Override
-					public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-						response.sendRedirect("/denied");
-					}
-				});
+				.csrf().disable();
 
 
 
