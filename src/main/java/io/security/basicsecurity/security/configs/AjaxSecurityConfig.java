87eefc488aa18cb66fc69e582181cd70a3f580c1
hyeonse <dlgustp1487@naver.com>
2022-09-09 14:46:17 +0900

    3) 인증 처리자 - AjaxAuthenticationProvider - 진행중 AjaxAuthenticationProvider를 /api/login실행시 못찾음

--- src/main/java/io/security/basicsecurity/security/configs/AjaxSecurityConfig.java
+++ src/main/java/io/security/basicsecurity/security/configs/AjaxSecurityConfig.java
@@ -1,11 +1,16 @@
 package io.security.basicsecurity.security.configs;
 
 import io.security.basicsecurity.security.filter.AjaxLoginProcessingFilter;
+import io.security.basicsecurity.security.provider.AjaxAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -13,21 +18,39 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class AjaxSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(ajaxAuthenticationProvider());
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
                 .antMatcher("/api/**")
                 .authorizeRequests()
+                .antMatchers("/api/login").permitAll()
                 .anyRequest().authenticated()
-        .and()
-                .addFilterBefore(ajaxLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class);
+                .and()
+                .addFilterBefore(ajaxLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class)
+        ;
+
         http.csrf().disable();
+
+    }
+
+    protected AjaxLoginProcessingFilter ajaxLoginProcessingFilter() throws Exception {
+        AjaxLoginProcessingFilter filter = new AjaxLoginProcessingFilter();
+        filter.setAuthenticationManager(authenticationManagerBean());
+        return filter;
     }
 
     @Bean
-    public AjaxLoginProcessingFilter ajaxLoginProcessingFilter() throws Exception {
-        AjaxLoginProcessingFilter ajaxLoginProcessingFilter = new AjaxLoginProcessingFilter();
-        ajaxLoginProcessingFilter.setAuthenticationManager(authenticationManagerBean());
-        return ajaxLoginProcessingFilter;
+    public AuthenticationProvider ajaxAuthenticationProvider(){
+        return new AjaxAuthenticationProvider(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
--- src/main/java/io/security/basicsecurity/security/provider/AjaxAuthenticationProvider.java
+++ src/main/java/io/security/basicsecurity/security/provider/AjaxAuthenticationProvider.java
@@ -5,7 +5,6 @@ import io.security.basicsecurity.security.service.AccountContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -34,27 +33,19 @@ public class AjaxAuthenticationProvider implements AuthenticationProvider {
 
         if(!passwordEncoder.matches(password, accountContext.getAccount().getPassword())) {
             throw new BadCredentialsException("Invalid Password");
-//      throw new BadCredentialsException("BadCredentialsException");
         }
 
         String secretKey = ( (FormWebAuthenticationDetails) authentication.getDetails() ).getSecretKey();
-//    FormWebAuthenticationDetails formWebAuthenticationDetails = (FormWebAuthenticationDetails) authentication.getDetails();
-//    String secretKey = formWebAuthenticationDetails.getSecretKey();
 
         if(secretKey == null || ! secretKey.equals("secret")) {
-//    if(secretKey == null || !"secret".equals(secretKey)) {
             throw new IllegalArgumentException("invalid Secret");
-//      throw new InsufficientAuthenticationException("InsufficientAuthenticationException");
         }
 
         return new UsernamePasswordAuthenticationToken(accountContext.getAccount(), null, accountContext.getAuthorities());
-//    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(accountContext.getAccount(), null, accountContext.getAuthorities());
-//    return authenticationToken;
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
         return authentication.equals(UsernamePasswordAuthenticationToken.class);
-//    return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
     }
 }
